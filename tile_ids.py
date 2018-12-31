ef81a24c9d8e012d65608f09d09e5ae0180fa11f
Jonathan Buchanan <johnyburd@gatech.edu>
2018-12-31 15:50:03 -0500

    hello

---
+++ tile_ids.py
@@ -0,0 +1,7 @@
+import pygame
+tile_ids = {
+    'dirt'   : pygame.image.load('res/dirt.png'),
+    'grass' : pygame.image.load('res/grass.png'),
+    'water' : pygame.image.load('res/water.png'),
+    'coal'  : pygame.image.load('res/coal.png')
+}
