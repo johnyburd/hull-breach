38b374ce3f75169caa667bae8e540bf6fa30d169
Jonathan Buchanan <johnyburd@gatech.edu>
2019-01-04 16:32:09 -0500

    no weird lines

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ clock = pygame.time.Clock()
 
 myfont = pygame.font.SysFont('Comic Sans MS', 40)
 
-message_log = "hello\nthis is a message\nthis is cool"
+message_log = ["hello","this is a message","this is cool"]
 
 TILESIZE  = 30
 MAPWIDTH  = 50
@@ -23,14 +23,12 @@ from tile_ids import tile_ids, entity_ids
 scaled_tile_ids = {}
 scaled_entity_ids = {}
 for k, i in tile_ids.items():
-    scaled_tile_ids[k] = pygame.transform.scale(i, (int(TILESIZE), int(TILESIZE)))
+    scaled_tile_ids[k] = pygame.transform.scale(i, (TILESIZE, TILESIZE))
 for k, i in entity_ids.items():
-    scaled_entity_ids[k] = pygame.transform.scale(i, (int(TILESIZE), int(TILESIZE)))
+    scaled_entity_ids[k] = pygame.transform.scale(i, (TILESIZE, TILESIZE))
 
 while True:
     clock.tick(30)
-    #print(screen.get_width()) # 1500
-    #print(screen.get_height()) # 900
     dirty_tiles = []
 
     for event in pygame.event.get():
@@ -52,14 +50,12 @@ while True:
         elif event.type == pygame.VIDEORESIZE:
             print(event.size)
             old_screen = screen
-            #MAPWIDTH = int(event.w / 30)
-            #MAPHEIGHT = int(event.h / 30)
-            TILESIZE = min(event.w / MAPWIDTH, event.h / MAPHEIGHT)
-            print(TILESIZE)
+            TILESIZE = int(min(event.w / MAPWIDTH, event.h / MAPHEIGHT))
+            print(int(TILESIZE))
             for k, i in tile_ids.items():
-                scaled_tile_ids[k] = pygame.transform.scale(i, (int(TILESIZE), int(TILESIZE)))
+                scaled_tile_ids[k] = pygame.transform.scale(i, (TILESIZE, TILESIZE))
             for k, i in entity_ids.items():
-                scaled_entity_ids[k] = pygame.transform.scale(i, (int(TILESIZE), int(TILESIZE)))
+                scaled_entity_ids[k] = pygame.transform.scale(i, (TILESIZE, TILESIZE))
             screen = pygame.display.set_mode((event.w,event.h), pygame.RESIZABLE)
             dirty_tiles.append(pygame.Rect(0,0, event.w, event.h))
             old_map = [[None for i in range(MAPWIDTH)] for j in range(MAPHEIGHT)]
@@ -69,7 +65,6 @@ while True:
 
     for row in range(MAPHEIGHT):
         for column in range(MAPWIDTH):
-            #draw the resource at that position in the tilemap, using the correct image
             if tilemap[row][column] != old_map[row][column]:
                 dirty_tiles.append(pygame.Rect(column * TILESIZE, row * TILESIZE, TILESIZE, TILESIZE))
                 screen.blit(scaled_tile_ids[tilemap[row][column]], (column*TILESIZE,row*TILESIZE))
@@ -77,13 +72,10 @@ while True:
                 screen.blit(scaled_entity_ids[entitymap[row][column]], (column*TILESIZE,row*TILESIZE))
                 dirty_tiles.append(pygame.Rect(column * TILESIZE, row * TILESIZE, TILESIZE, TILESIZE))
             count = 0
-            for line in message_log.split('\n'):
+            for line in message_log:
                 textsurface = myfont.render(line, False, (0, 0, 0))
                 screen.blit(textsurface,(0,count))
                 count += 40
     old_map = tilemap
 
-    #update the display
-    #print(dirty_tiles)
     pygame.display.update(dirty_tiles)
-    #time.sleep(30.0 - ((time.time() - starttime) % 30.0))
